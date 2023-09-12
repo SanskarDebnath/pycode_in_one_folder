a7253fb2da12f4870e6d4c06265bb8b623848cd4
sanskar0381 <sanskardebnath2019@gmail.com>
2023-09-12 23:44:37 +0530

    .py

--- randomnumber1to10.py
+++ randomnumber1to10.py
@@ -12,12 +12,12 @@ def generate_random_name(length):
 min_num = 100000000000000
 max_num = 999999999999999
 random_numbers = ["TCEA-CN-" + str(random.randint(min_num, max_num)) for _ in range(100)]
-print(random_numbers)
+# print(random_numbers)
 
 num_names = 100
 name_length = 15
 random_names = [generate_random_name(name_length) for _ in range(num_names)]
-print(random_names)
+# print(random_names)
 
 data = list(zip(random_names, random_numbers, just_rand_str))
 
@@ -26,6 +26,5 @@ with open("numbers_and_names.csv", "w", newline="") as csvfile:
     file.writerow(["Name", "Number", "Action"])
     for name, number, action in data:
         file.writerow([name, number, action])
-    print("saved")
 
-#print ("final done")
+print("Done")
